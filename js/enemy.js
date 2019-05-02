aa010219f09ab71ae5bad65ad4b0fabce2e7800a
dhagandev <dhagan@utexas.edu>
2019-05-01 21:50:01 -0500

    Create enemy class

---
+++ js/enemy.js
@@ -0,0 +1,12 @@
+import * from 'character.js'
+
+class Enemy extends Character {
+	let health = null;
+	let coinsDropped = null;
+
+	constructor(name, image, health, coinsDropped) {
+		super(name, image);
+		this.health = health;
+		this.coinsDropped = coinsDropped;
+	}
+}
