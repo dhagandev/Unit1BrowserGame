72c77f1f8dd751891022b4805d991e5f1f207b27
dhagandev <dhagan@utexas.edu>
2019-05-02 11:17:17 -0500

    Create stats class

---
+++ js/stats.js
@@ -0,0 +1,17 @@
+class Stats {
+	let dmgDealt = null;
+	let moneyHand = null;
+	let moneyEarned = null;
+	let numKilled = null;
+	let upgradesBought = null;
+	let achievementsEarned = null;
+
+	constructor () {
+		dmgDealt = 0;
+		moneyHand = 0;
+		moneyEarned = 0;
+		numKilled = 0;
+		upgradesBought = [];
+		achievementsEarned = [];
+	}
+}
