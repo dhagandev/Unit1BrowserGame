55fa08cf6fe0e1d010eb83b8048da64347250c6b
dhagandev <dhagan@utexas.edu>
2019-05-04 08:16:24 -0500

    Enemy Image display on fight arena

--- js/minotaur.js
+++ js/minotaur.js
@@ -2,7 +2,7 @@ class Minotaur extends Enemy {
 	constructor() {
 		let health = 35;
 		let coinsDropped = 0;
-		super("Minotaur", "./images/minotaur.js", health, coinsDropped);
+		super("Minotaur", "./images/minotaur.png", health, coinsDropped);
 		coinsDropped = super.getCoins(9, 14);
 		super.coinsDropped = coinsDropped;
 	}
