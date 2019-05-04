b1881b1969240e53de8a09405ebfe13fd384318b
dhagandev <dhagan@utexas.edu>
2019-05-03 20:17:09 -0500

    Move upgrade cards to upgrade js to build them based off passed in information.

---
+++ js/allUpgrades.js
@@ -0,0 +1,31 @@
+let upgradeList = `{
+	"upgrades": [
+		{
+			"name": "Wooden Sword",
+			"flavorText": "Here, take this with you!",
+			"cost": 20,
+			"descrip": "+1 Attack Strength to your hero.",
+			"charAffected": "Hero",
+			"thingAffected": "Attack Strength",
+			"effect": 1
+		},
+		{
+			"name": "Basket o Onions",
+			"flavorText": "Your breath can kill. Literally.",
+			"cost": 50,
+			"descrip": "+2 Attack Strength to your hero.",
+			"charAffected": "Hero",
+			"thingAffected": "Attack Strength",
+			"effect": 2
+		},
+		{
+			"name": "Bard's Song",
+			"flavorText": "The shrill of the bard encourages you to run faster, even if it is just to get away.",
+			"cost": 30,
+			"descrip": "-0.25 seconds from Attack Speed of your fellow adventurer.",
+			"charAffected": "Fellow Adventurer",
+			"thingAffected": "Attack Speed",
+			"effect": -0.25
+		}
+	]
+}`
--- js/upgrades.js
+++ js/upgrades.js
@@ -1,8 +1,45 @@
 class Upgrades {
 	allUpgrades = null;
+	gmObject = null
 
-	constructor() {
+	constructor(gm) {
 		//Determine the best way to store the upgrades
 		//That will lead to how to modify elements
+		this.gmObject = gm;
+		this.allUpgrades = JSON.parse(upgradeList).upgrades;
+		this.createUpgradeCard();
+	}
+
+	createUpgradeCard() {
+		let upgradesSection = document.querySelector(".upgrades");
+		for (let i = 0; i < this.allUpgrades.length; i++) {
+			let upgradeObj = this.allUpgrades[i];
+			let upgradeCard = document.createElement("div");
+			upgradeCard.classList.add("upgrade-card");
+
+			let upgradeName = document.createElement("div");
+			upgradeName.classList.add("upgrade-name");
+			upgradeName.innerHTML = upgradeObj.name;
+
+			let upgradeCost = document.createElement("div");
+			upgradeCost.classList.add("upgrade-cost");
+			upgradeCost.innerHTML = upgradeObj.cost;
+
+			let upgradeDescription = document.createElement("div");
+			upgradeDescription.classList.add("upgrade-description");
+			upgradeDescription.innerHTML = upgradeObj.descrip;
+
+			let upgradeButton = document.createElement("div");
+			upgradeButton.classList.add("buy-upgrade");
+			upgradeButton.innerHTML = "Buy";
+
+			upgradeCard.append(upgradeName);
+			upgradeCard.append(upgradeCost);
+			upgradeCard.append(upgradeDescription);
+			upgradeCard.append(upgradeButton);
+
+			upgradesSection.append(upgradeCard);
+		}
+		console.log(upgradesSection);
 	}
 }
